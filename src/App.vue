0034777b378b9951adf313d76575620a6da4ab70
CristianCala <cristiancala@protonmail.com>
2023-08-28 11:36:16 -0400

    [base files]Add tailwind & preprocesors

--- src/App.vue
+++ src/App.vue
@@ -4,82 +4,43 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+<div class="bg-[#F9FAFB] h-screen w-screen flex items-center">
+        <div class="h-max mx-auto flex flex-col items-center">
+            <img class="h-[40px] w-[47px] mb-5" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+            <h1 class="text-xl font-bold text-center pb-10">Sign in to your account</h1>
+            <div class="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
+                <div>
+                    <label class="text-gray-600 font-bold inline-block pb-2 " for="email">Email</label>
+                    <input class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" type="email" name="email" placeholder="mehedi@jaman.com">
+                </div>
+                <div>
+                    <label class="text-gray-600 font-bold inline-block pb-2" for="password">Password</label>
+                    <input class="border border-gray-400 focus:outline-slate-400 rounded-md w-full shadow-sm px-5 py-2" type="password" name="password" placeholder="******">
+                </div>
+                <div class="flex">
+                    <div class="w-1/2">
+                        <input type="checkbox" name="remeberMe">
+                        <label for="remeberMe">Remeber me</label>
+                    </div>
+                    <div class="w-1/2">
+                        <a class="font-bold text-blue-600" href="">Forgot password ?</a>
+                    </div>
+                </div>
+                <div>
+                    <input class="bg-[#4F46E5] w-full py-2 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]" type="submit" value="Login" >
+                </div>
+                <div>
+                    <p class="text-center">Or continue with</p>
+                </div>
+                <div class="flex gap-4">
+                    <button class="bg-[#1D9BF0] w-1/2 py-1 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]">Twitter</button>
+                    <button class="bg-[#24292F] w-1/2 py-1 rounded-md text-white font-bold cursor-pointer hover:bg-[#181196]">Github</button>
+                </div>
+            </div>
+            <p class="text-sm text-gray-500 mt-10">Not a member? <a href="#" class="text-[#4F46E5] font-bold">Start a 14 day free trial</a></p>
+        </div>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
