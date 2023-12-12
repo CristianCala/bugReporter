529aa8c2ef0a8f4609f5f117de8d944f7a78c7aa
CristianCala <cristiancala@protonmail.com>
2023-12-12 18:10:12 -0400

    [base]Adding base files and config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<div class="flex items-center justify-center h-screen">
+		<div class="max-w-sm rounded overflow-hidden shadow-lg">
+			<div class="px-6 py-4 text-center">
+				<div class="font-bold text-2xl mb-2">Welcome to Bug Reporter</div>
+				<p class="text-gray-900">
+					A report bug system for projects
+				</p>
+				<router-link
+					to="/projects"
+					class="bg-black text-white rounded-md font-bold py-2 px-28 mt-4 inline-block hover:bg-gray-700"
+				>
+					Get Started
+				</router-link>
+			</div>
+		</div>
+	</div>
 </template>
+
+<script>
+export default {
+    name: 'HomeView',
+    data() {
+        return {
+            message: 'Hello World!'
+        }
+    }
+}
+</script>
